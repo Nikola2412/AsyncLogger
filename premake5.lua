84ce07fcfe55e54cc3a0d2a2244d726cfc02686f
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2026-03-23 21:57:28 +0100

    test

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "AsyncLogger"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"AsyncLogger/**.h",
+		"AsyncLogger/**.cpp",
+		"AsyncLogger/Logger/**.h",
+		"AsyncLogger/Logger/**.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
